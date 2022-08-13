0afdf7c7f366656843f4ed9866af10e69fe2e63c
dereckdemezquita <dereck@demezquita.com>
2022-08-13 18:47:22 -0500

    Now have ball class with methods, ball moves in direction of mouse, click to speed up and remove speed.

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,22 +1,41 @@
 
 import { Vec2 } from "./modules/math";
+import { Ball } from "./modules/ball";
 
 const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
-const ball = {
-    position: new Vec2(10, 5),
-    drawPos: new Vec2(10, 5),
-    velocity: new Vec2(1, 1)
-}
+
+const ball = new Ball(10, 10, 5, 1, 1, 0.5);
 
 let mousePos: Vec2 = new Vec2(1, 1);
-const physicsInterval = 100;
+const physicsInterval = 15;
 const tweenInterval = 15;
+
+document.body.addEventListener('contextmenu', e => {
+    e.preventDefault();
+});
 
 // get mouse position from canvas
 canvas.addEventListener('mousemove', (e) => {
     mousePos = new Vec2(e.offsetX, e.offsetY);
+});
+
+canvas.addEventListener('mousedown', (e) => {
+    const butt = e.button;
+
+    switch(butt) {
+        case 0:
+            if(ball.velocity.magnitude === 0) ball.applyForce(ball.position.direction(mousePos));
+            ball.addEnergy(200);
+            break;
+        case 2:
+            ball.removeEnergy(200);
+            break;
+    }
+
+    console.log(ball.energy);
+    console.log(ball.velocity.magnitude);
 });
 
 function animate() {
@@ -26,25 +45,28 @@ function animate() {
     ctx.clearRect(0, 0, 800, 800);
     // draw the ball
     ctx.beginPath();
-    ctx.arc(ball.drawPos.x, ball.drawPos.y, 5, 0, Math.PI * 2);
+    ctx.arc(ball.drawPosition.x, ball.drawPosition.y, ball.radius, 0, Math.PI * 2);
     ctx.fillStyle = '#ff0000';
     ctx.fill();
 }
 
-function tween() {
-    ball.drawPos = ball.drawPos.add(ball.velocity.multiply(tweenInterval / physicsInterval));
-}
+// function tween() {
+//     ball.drawPos = ball.drawPos.add(ball.velocity.multiply(tweenInterval / physicsInterval));
+// }
 
 function physicsUpdate() {
+    // have the ball follow the mouse
     const direction: Vec2 = mousePos.subtract(ball.position).normalise();
-    ball.velocity = direction.multiply(25);
 
-    ball.velocity = ball.velocity.add(direction);
+    // make the ball move faster by scalar
+    ball.setDirection(direction);
+
+    // update the ball's position
     ball.position = ball.position.add(ball.velocity);
-    ball.drawPos = ball.position;
+    ball.drawPosition = ball.position;
 }
 
 setInterval(physicsUpdate, physicsInterval);
-setInterval(tween, tweenInterval);
+// setInterval(tween, tweenInterval);
 
 animate();
--- src/ts/modules/ball.ts
+++ src/ts/modules/ball.ts
@@ -1,6 +1,80 @@
 
-// import Vec2 from "./math";
+import { Vec2 } from "./math";
 
-// class Ball {
+export class Ball {
+    energy: number = 0;
+    position; drawPosition; velocity; mass; elasticity; colour; radius;
 
-// }
+    // radius and velocity have to be passed
+    constructor(
+        x: number = 0,
+        y: number = 0,
+        radius: number = 5,
+        vX: number = 0,
+        vY: number = 0,
+        elasticity: number = 1,
+        colour: string = '#ff0000'
+    ) {
+        this.position = new Vec2(x, y);
+        this.radius = radius;
+        this.drawPosition = new Vec2(x, y);
+        this.velocity = new Vec2(vX, vY);
+        this.colour = colour;
+
+        if (elasticity > 1) {
+            this.elasticity = 1;
+        } else if (elasticity < 0) {
+            this.elasticity = 0;
+        }
+
+        this.mass = radius ** 2 * Math.PI;
+        this.calculateEnergy();
+    }
+
+    // takes a number to add energy to the ball
+    // we are adding to the normalised vector of the velocity
+    addEnergy(energy: number): void {
+        this.energy += energy;
+        this.calculateVelocity();
+    }
+
+    removeEnergy(energy: number): void {
+        const newEnergy = this.energy - energy;
+
+        if (newEnergy < 0) {
+            this.energy = 0;
+        } else {
+            this.energy = newEnergy;
+        }
+
+        this.calculateVelocity();
+    }
+
+    // kinectic energy is calculated by m/2 * velocity ^ 2
+    // we are calculating the magnitude of the ball
+    // reminder magnitude is the lenght of a vector
+    // then we can apply (multiply) the magnitude to the velocity after it is normalised
+    calculateVelocity(): void {
+        // e = mc^2
+        // c = sqrt(e / m / 2)
+        const newMagnitude = Math.sqrt(this.energy / (this.mass / 2));
+
+        (Math.round(newMagnitude) == 0 ? this.velocity = new Vec2() : this.velocity = this.velocity.normalise().multiply(newMagnitude));
+    }
+
+    calculateEnergy(): void {
+        // e = (c^2) * m / 2
+        this.energy = (this.velocity.magnitude ** 2) * (this.mass / 2);
+    }
+
+    applyForce(force: Vec2): void {
+        this.velocity = this.velocity.add(force);
+        this.calculateEnergy();
+    }
+
+    // changes the direction without affecting the magnitude
+    // so no need to update energy etc
+    setDirection(direction: Vec2): void {
+        this.velocity = direction.multiply(this.velocity.magnitude);
+    }
+}
