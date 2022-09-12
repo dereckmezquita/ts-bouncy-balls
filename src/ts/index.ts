421b4e24b59954391438b932c91fa89c7c90c058
dereckdemezquita <dereck@demezquita.com>
2022-09-11 22:17:39 -0500

    Toggle menu on off.

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -4,3 +4,10 @@ import { GameManager } from "./modules/GameManager";
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 
 const game = new GameManager(canvas);
+
+// when click on button class "toggle-menu" show menu
+(document.querySelector('.toggle-menu') as HTMLElement).addEventListener('click', () => {
+    // style display none or block
+    (document.querySelector('.stats') as HTMLElement).style.display = (document.querySelector('.stats') as HTMLElement).style.display === 'none' ? 'block' : 'none';
+
+})
