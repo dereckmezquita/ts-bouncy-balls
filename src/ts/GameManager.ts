f9e931753699f38e43d4f6cec17684c82b42b77e
dereckmezquita <dereck@mezquita.io>
2023-12-27 11:08:57 -0600

    Better user settings inputs; event listeners.

--- src/ts/GameManager.ts
+++ src/ts/GameManager.ts
@@ -216,82 +216,44 @@ export class GameManager {
 
     // ------------------------------
     updateUserSettings(): void {
-        // ------------------------------
-        // ball stats
-        const numBalls = document.querySelector('.num-balls') as HTMLDivElement;
-        numBalls.innerHTML = `Balls: ${this.balls.length}; Average speed: ${this.getAverageSpeed}; Average energy: ${this.getAverageEnergy}`;
+        const updateParams = () => {
+            // Update universe parameters
+            this.universeParams.mouseAttractor = (document.querySelector("#mouse-cog") as HTMLInputElement).checked;
+            this.universeParams.ballsAttractor = (document.querySelector("#inter-ball-cog") as HTMLInputElement).checked;
+            this.universeParams.gravity = parseFloat((document.querySelector('.gravity-slider-value') as HTMLInputElement).value);
+            this.universeParams.interBallAttraction = parseFloat((document.querySelector('.inter-ball-gravity-value') as HTMLInputElement).value);
+    
+            // Update ball parameters
+            this.ballParams.radius = parseInt((document.querySelector('.ball-radius-value') as HTMLInputElement).value);
+            this.ballParams.elasticity = parseFloat((document.querySelector('.ball-elasticity-value') as HTMLInputElement).value);
+    
+            // Update user parameters
+            this.userParams.spawnSelector = (document.querySelector('#spawn-ball') as HTMLInputElement).checked ? "ball" : "blackHole";
+            this.userParams.addEnergySlider = parseInt((document.querySelector('.energy-slider-value') as HTMLInputElement).value);
+            this.userParams.addRemoveNumBalls = parseInt((document.querySelector('.add-n-balls-slider-value') as HTMLInputElement).value);
+    
+            // Update physics interval
+            this.physicsInterval = parseInt((document.querySelector('.time-slider-value') as HTMLInputElement).value);
+    
+            // Update innerHTML for display elements
+            (document.querySelector('.num-balls') as HTMLDivElement).innerHTML = `Balls: ${this.balls.length}; Average speed: ${this.getAverageSpeed}; Average energy: ${this.getAverageEnergy}`;
+            (document.querySelector('.ball-radius-label') as HTMLDivElement).innerHTML = `Ball radius (${this.ballParams.radius})`;
+            (document.querySelector('.ball-elasticity-label') as HTMLDivElement).innerHTML = `Ball elasticity (${this.ballParams.elasticity})`;
+            (document.querySelector('.energy-slider-label') as HTMLDivElement).innerHTML = `Add energy (${this.userParams.addEnergySlider})`;
+            (document.querySelector('.add-n-balls-slider-label') as HTMLDivElement).innerHTML = `Add/delete n balls (${this.userParams.addRemoveNumBalls})`;
+        };
+    
+        document.querySelectorAll("input").forEach(element => {
+            element.addEventListener("change", updateParams);
+            element.addEventListener("input", updateParams);
+        });
+    
+        updateParams();
+    }
 
-        // check if user wants to spawn ball or gravity centre
-        const spawnSelector_ball = document.querySelector('#spawn-ball') as HTMLInputElement;
-        this.userParams.spawnSelector = spawnSelector_ball.checked ? "ball" : "blackHole";
-
-        // ------------------------------
-        // ball settings
-        const ballRadius_label = document.querySelector('.ball-radius-label') as HTMLInputElement;
-        ballRadius_label.innerHTML = `Ball radius (${this.ballParams.radius})`;
-        // ball elasticity
-        const ballElasticity_label = document.querySelector('.ball-elasticity-label') as HTMLInputElement;
-        ballElasticity_label.innerHTML = `Ball elasticity (${this.ballParams.elasticity})`;
-
-        // ------------------------------
-        // universe settings
-        const addEnergySlider_label = document.querySelector('.energy-slider-label') as HTMLInputElement;
-        addEnergySlider_label.innerHTML = `Add energy (0):`
-        // --------------
-        const addRemoveNumBalls = document.querySelector('.add-n-balls-slider-label') as HTMLInputElement;
-        addRemoveNumBalls.innerHTML = `Add/delete n balls (1):`
-        // --------------
-        // time
-        const timeSlider_label = document.querySelector('.time-slider-label') as HTMLInputElement;
-        timeSlider_label.innerHTML = `Time ms:`
-
-        setInterval(() => {
-            // ball stats
-            numBalls.innerHTML = `Balls: ${this.balls.length}; Average speed: ${this.getAverageSpeed}; Average energy: ${this.getAverageEnergy}`;
-
-            this.userParams.spawnSelector = spawnSelector_ball.checked ? "ball" : "blackHole";
-            const mouseAttractor_value = document.querySelector("#mouse-cog") as HTMLInputElement;
-            this.universeParams.mouseAttractor = mouseAttractor_value.checked;
-
-            const interBallAttractor_value = document.querySelector("#inter-ball-cog") as HTMLInputElement;
-            this.universeParams.ballsAttractor = interBallAttractor_value.checked;
-
-            // ------------------------------
-            // ball radius
-            ballRadius_label.innerHTML = `Ball radius (${this.ballParams.radius})`;
-            const ballRadius_value = document.querySelector('.ball-radius-value') as HTMLInputElement;
-            this.ballParams.radius = parseInt(ballRadius_value.value);
-            // ball elasticity
-            ballElasticity_label.innerHTML = `Ball elasticity (${this.ballParams.elasticity})`;
-            const ballElasticity_value = document.querySelector('.ball-elasticity-value') as HTMLInputElement;
-            this.ballParams.elasticity = parseFloat(ballElasticity_value.value);
-
-            // ------------------------------
-            // universe settings
-            // add energy to all balls
-            addEnergySlider_label.innerHTML = `Add energy (${this.userParams.addEnergySlider}):`
-            const addEnergySlider_value = document.querySelector('.energy-slider-value') as HTMLInputElement;
-            this.userParams.addEnergySlider = parseInt(addEnergySlider_value.value);
-
-            // number of balls per click
-            addRemoveNumBalls.innerHTML = `Add/delete n balls (${this.userParams.addRemoveNumBalls}):`
-            const addRemoveNumBalls_value = document.querySelector('.add-n-balls-slider-value') as HTMLInputElement;
-            this.userParams.addRemoveNumBalls = parseInt(addRemoveNumBalls_value.value);
-
-            // --------------
-            // gravity
-            const gravitySlider_value = document.querySelector('.gravity-slider-value') as HTMLInputElement;
-            this.universeParams.gravity = parseFloat(gravitySlider_value.value);
-            // --------------
-            // inter ball gravity
-            const interBallGravity_value = document.querySelector('.inter-ball-gravity-value') as HTMLInputElement;
-            this.universeParams.interBallAttraction = parseFloat(interBallGravity_value.value);
-
-            // time
-            timeSlider_label.innerHTML = `Time ms:`
-            const timeSlider_value = document.querySelector('.time-slider-value') as HTMLInputElement;
-            this.physicsInterval = parseInt(timeSlider_value.value);
-        }, 250);
+    updateGameStats(): void {
+        const numBallsElement = document.querySelector('.num-balls') as HTMLDivElement;
+        numBallsElement.innerHTML = `Balls: ${this.balls.length}; Average speed: ${this.getAverageSpeed}; Average energy: ${this.getAverageEnergy}`;
     }
 
     // ------------------------------
@@ -319,6 +281,8 @@ export class GameManager {
         for (const cog of this.blackHoles) {
             Ball.draw(this.ctx, cog);
         }
+
+        this.updateGameStats();
     }
 
     // ------------------------------
